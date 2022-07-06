051ffdd8aa0150d2acac768c1c9d1d4fed5ef068
SevioCorrea <102820124+SevioCorrea@users.noreply.github.com>
2022-07-06 20:04:38 -0300

    V1

---
+++ EggTimer/ViewController.swift
@@ -0,0 +1,34 @@
+//
+//  ViewController.swift
+//  EggTimer
+//
+//  Created by Sévio Corrêa on 06/07/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    let eggTimes = ["Soft": 5, "Medium": 8, "Hard": 12]
+    
+    var timer = Timer()
+    
+    @IBAction func hardnessSelectes(_ sender: UIButton) {
+        
+        timer.invalidate()
+        
+        print(eggTimes[sender.currentTitle!] ?? "Error")
+        
+        var secondsRemaining = eggTimes[sender.currentTitle!]! * 60
+            
+        timer = Timer.scheduledTimer(withTimeInterval: 1.0, repeats: true) { (Timer) in
+            if secondsRemaining > 0 {
+                print ("\(secondsRemaining) seconds")
+                secondsRemaining -= 1
+            } else {
+                Timer.invalidate()
+            }
+        }
+    }
+    
+}
